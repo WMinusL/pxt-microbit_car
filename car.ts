c15ad8f92923dff6b422d546380cfa20bd9d3b80
WMinusL <70479503+WMinusL@users.noreply.github.com>
2023-12-19 13:39:49 +0800

    更新 car.ts

--- car.ts
+++ car.ts
@@ -4,15 +4,12 @@
 //% weight=100 color=#0fbc11 icon="\uf021"
 namespace microbit_car {
     let _DEBUG: boolean = false
-    const debug = (msg: string) => {
-        if (_DEBUG === true) {
-            serial.writeLine(msg)
-        }
-    }
+
+    let CHIP_ADDR = 64
 
     const MIN_CHIP_ADDRESS = 0x40
     const MAX_CHIP_ADDRESS = MIN_CHIP_ADDRESS + 62
-    const chipResolution = 4096;
+    const chipResolution = 4096
     const PrescaleReg = 0xFE //the prescale register address
     const modeRegister1 = 0x00 // MODE1
     const modeRegister1Default = 0x01
@@ -89,6 +86,17 @@ namespace microbit_car {
         MotorRR = 3,    
     }
 
+    export enum MoveDir {
+        deg0 =  0,  
+        deg45 = 45,
+        deg90 = 90,
+        deg135 = 135,
+        deg180 = 180,
+        deg225 = 225,
+        deg270 = 270,
+        deg315 = 315,
+    }
+
     export class ServoConfigObject {
         id: number;
         pinNumber: number;
@@ -121,14 +129,6 @@ namespace microbit_car {
             this.pinNumber = config.pinNumber > -1 ? config.pinNumber : this.id - 1
             this.setOffsetsFromFreq(config.minOffset, config.maxOffset, config.midOffset)
             this.position = -1
-        }
-
-        debug() {
-            const params = this.config()
-
-            for (let j = 0; j < params.length; j = j + 2) {
-                debug(`Servo[${this.id}].${params[j]}: ${params[j + 1]}`)
-            }
         }
 
         setOffsetsFromFreq(startFreq: number, stopFreq: number, midFreq: number = -1): void {
@@ -198,11 +198,10 @@ namespace microbit_car {
          */
     //% block
     export function init(chipAddress: number = 0x40, newFreq: number = 50) {
-        debug(`Init chip at address ${chipAddress} to ${newFreq}Hz`)
         const buf = pins.createBuffer(2)
         const freq = (newFreq > 1000 ? 1000 : (newFreq < 40 ? 40 : newFreq))
         const prescaler = calcFreqPrescaler(freq)
-
+        CHIP_ADDR = chipAddress
         write(chipAddress, modeRegister1, sleep)
 
         write(chipAddress, PrescaleReg, prescaler)
@@ -231,11 +230,9 @@ namespace microbit_car {
    function getChipConfig(address: number): ChipConfig {
         for (let i = 0; i < chips.length; i++) {
             if (chips[i].address === address) {
-                debug(`Returning chip ${i}`)
                 return chips[i]
             }
         }
-        debug(`Creating new chip for address ${address}`)
         const chip = new ChipConfig(address)
         const index = chips.length
         chips.push(chip)
@@ -260,9 +257,6 @@ namespace microbit_car {
         onStep = Math.max(0, Math.min(4095, onStep))
         offStep = Math.max(0, Math.min(4095, offStep))
 
-        debug(`setPinPulseRange(${pinNumber}, ${onStep}, ${offStep}, ${chipAddress})`)
-        debug(`  pinOffset ${pinOffset}`)
-
         // Low byte of onStep
         write(chipAddress, pinOffset + channel0OnStepLowByte, onStep & 0xFF)
 
@@ -278,18 +272,16 @@ namespace microbit_car {
 
     /**
      * Used to set the duty cycle (0-100) of a given led connected to the PCA9685
-     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
      * @param ledNumber The number (0-15) of the LED to set the duty cycle on
      * @param dutyCycle The duty cycle (0-100) to set the LED to
      */
     //% block
     //% subcategory=Servo/Motor
-    export function setLedDutyCycle(ledNum: LEDNum = 0, dutyCycle: number, chipAddress: number = 0x40): void {
+    export function setLedDutyCycle(ledNum: LEDNum = 0, dutyCycle: number): void {
         ledNum = Math.max(0, Math.min(15, ledNum))
         dutyCycle = Math.max(0, Math.min(100, dutyCycle))
         const pwm = (dutyCycle * (chipResolution - 1)) / 100
-        debug(`setLedDutyCycle(${ledNum}, ${dutyCycle}, ${chipAddress})`)
-        return setPinPulseRange(<number>ledNum, 0, pwm, chipAddress)
+        return setPinPulseRange(<number>ledNum, 0, pwm, CHIP_ADDR)
     }
 
     function degrees180ToPWM(freq: number, degrees: number, offsetStart: number, offsetEnd: number): number {
@@ -304,42 +296,39 @@ namespace microbit_car {
 
     /**
      * Used to move the given servo to the specified degrees (0-180) connected to the PCA9685
-     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
      * @param servoNum The number (1-16) of the servo to move
      * @param degrees The degrees (0-180) to move the servo to
      */
     //% block
     //% subcategory=Servo/Motor
-    export function setServoPosition(servoNum: PWMNum = 8, degrees: number, chipAddress: number = 0x40): void {
-        const chip = getChipConfig(chipAddress)
+    export function setServoPosition(servoNum: PWMNum = 8, degrees: number): void {
+        const chip = getChipConfig(CHIP_ADDR)
         servoNum = Math.max(0, Math.min(15, servoNum))
         degrees = Math.max(0, Math.min(180, degrees))
         const servo: ServoConfig = chip.servos[servoNum]
         const pwm = degrees180ToPWM(chip.freq, degrees, servo.minOffset, servo.maxOffset)
         servo.position = degrees
-        servo.debug()
-        return setPinPulseRange(servo.pinNumber, 0, pwm, chipAddress)
+        return setPinPulseRange(servo.pinNumber, 0, pwm, CHIP_ADDR)
     }
 
     
     /**
      * Single Motor Control
      * @param speed [-100,100] percent of fullspeed, negative is reverse
-     * @param chipAddress [64,125] The I2C address of your PCA9685; eg: 64
      */
     //% block
     //% subcategory=Servo/Motor
-    export function MotorControl(motor: Motor,speed: number = 0, chipAddress: number = 0x40): void {
+    export function MotorControl(motor: Motor,speed: number = 0): void {
         speed = Math.max(-100, Math.min(100, speed))
         if(speed>0)
         {
-            setLedDutyCycle(2 * motor, 100 - Math.abs(speed), chipAddress)
-            setLedDutyCycle(2 * motor + 1, 100, chipAddress)
+            setLedDutyCycle(2 * motor, 100 - Math.abs(speed))
+            setLedDutyCycle(2 * motor + 1, 100)
         }
         else
         {
-            setLedDutyCycle(2 * motor + 1, 100 - Math.abs(speed), chipAddress)
-            setLedDutyCycle(2 * motor, 100, chipAddress)
+            setLedDutyCycle(2 * motor + 1, 100 - Math.abs(speed))
+            setLedDutyCycle(2 * motor, 100)
         }
     }
 
@@ -347,36 +336,34 @@ namespace microbit_car {
      * Car translation use Mecanum wheel
      * @param speed [0,100] percent of fullspeed
      * @param degrees [0,360] direction of translation
-     * @param chipAddress [64,125] The I2C address of your PCA9685; eg: 64
      */
-    //% block="Car Translation speed =$speed, degrees =$degrees, chipAddress =$chipAddress"
+    //% block="Car Translation speed =$speed, degrees =$degrees"
     //% subcategory=Servo/Motor
-    export function CarTranslation(speed: number = 0, degrees: number = 0, chipAddress: number = 0x40): void {
+    export function CarTranslation(speed: number = 0, degrees: MoveDir = MoveDir.deg0): void {
         speed = Math.max(0, Math.min(100, speed))
         degrees = Math.max(0, Math.min(360, degrees))
         const speed_lim = speed * Math.sin(Math.PI / 4)
         const rad = Math.PI*degrees/180
         const vx = speed_lim * Math.cos(rad)
         const vy = -speed_lim * Math.sin(rad)
-        MotorControl(Motor.MotorLF, vx - vy, chipAddress)
-        MotorControl(Motor.MotorRF, vx + vy, chipAddress)
-        MotorControl(Motor.MotorLR, vx + vy, chipAddress)
-        MotorControl(Motor.MotorRR, vx - vy, chipAddress)
+        MotorControl(Motor.MotorLF, vx - vy)
+        MotorControl(Motor.MotorRF, vx + vy)
+        MotorControl(Motor.MotorLR, vx + vy)
+        MotorControl(Motor.MotorRR, vx - vy)
     }
     
     /**
      * Car Rotation use Mecanum wheel
      * @param speed [-100,100] percent of fullspeed
-     * @param chipAddress [64,125] The I2C address of your PCA9685; eg: 64
      */
-    //% block="Car Rotation speed =$speed, chipAddress =$chipAddress"
+    //% block="Car Rotation speed =$speed"
     //% subcategory=Servo/Motor
-    export function CarRotation(speed: number = 0, chipAddress: number = 0x40): void {
+    export function CarRotation(speed: number = 0): void {
         speed = Math.max(0, Math.min(100, speed))
-        MotorControl(Motor.MotorLF, speed, chipAddress)
-        MotorControl(Motor.MotorRF, -speed, chipAddress)
-        MotorControl(Motor.MotorLR, speed, chipAddress)
-        MotorControl(Motor.MotorRR, -speed, chipAddress)
+        MotorControl(Motor.MotorLF,  speed)
+        MotorControl(Motor.MotorRF, -speed)
+        MotorControl(Motor.MotorLR,  speed)
+        MotorControl(Motor.MotorRR, -speed)
     }
 
 }
